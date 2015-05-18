a15c92b2704b3b1cd4cad8988680ee301a58fa7d
Daniel Orbach <daniel.isdev@gmail.com>
2015-05-18 22:16:46 +0300

    test on your computer

--- CalculatorProject/app/src/main/java/com/corp/isdev/calculator/MainActivity.java
+++ CalculatorProject/app/src/main/java/com/corp/isdev/calculator/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TOM you are a HOMO
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
