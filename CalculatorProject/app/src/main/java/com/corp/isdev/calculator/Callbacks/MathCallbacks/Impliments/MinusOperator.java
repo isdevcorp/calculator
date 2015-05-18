622bed2d8ebb590bf6cc31675407b234891f25c6
isdevcorp <isdev.corp@gmail.com>
2015-05-18 23:16:37 +0300

    Added the minus & plus impls

---
+++ CalculatorProject/app/src/main/java/com/corp/isdev/calculator/Callbacks/MathCallbacks/Impliments/MinusOperator.java
@@ -0,0 +1,13 @@
+package com.corp.isdev.calculator.Callbacks.MathCallbacks.Impliments;
+
+import com.corp.isdev.calculator.Callbacks.MathCallbacks.IOperator;
+
+/**
+ * Created by USER on 18/05/2015.
+ */
+public class MinusOperator implements IOperator {
+    @Override
+    public int calc(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+}
--- CalculatorProject/app/src/main/java/com/corp/isdev/calculator/Callbacks/MathCallbacks/OperatorFactory.java
+++ CalculatorProject/app/src/main/java/com/corp/isdev/calculator/Callbacks/MathCallbacks/OperatorFactory.java
@@ -1,13 +1,21 @@
 package com.corp.isdev.calculator.Callbacks.MathCallbacks;
 
+import com.corp.isdev.calculator.Callbacks.MathCallbacks.Impliments.MinusOperator;
+import com.corp.isdev.calculator.Callbacks.MathCallbacks.Impliments.PlusOperator;
+
 /**
  * Created by USER on 18/05/2015.
  */
 public class OperatorFactory {
-    public static IOperator getOperatorClass(OperatorEnum selectedOperator) {
+    public static IOperator getOperatorClass(OperatorEnum selectedOperator) throws IllegalArgumentException{
 
         switch (selectedOperator)
         {
+            case Plus:
+                return new PlusOperator();
+
+            case Minus:
+                return new MinusOperator();
 
             default:
                 throw new IllegalArgumentException(String.format("Selected operator (%s) does not exist in the system",selectedOperator.toString()));
