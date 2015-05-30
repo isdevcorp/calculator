1d69a7ce2a6d48ada2fc823937415a7b9c88a437
Daniel Orbach <daniel.isdev@gmail.com>
2015-05-30 23:22:47 +0300

    added operand command for numeric values

---
+++ CalculatorProject/app/src/main/java/com/corp/isdev/calculator/commands/OperandCommand.java
@@ -0,0 +1,18 @@
+package com.corp.isdev.calculator.commands;
+
+import android.view.View;
+
+/**
+ * Created by daniel on 5/30/15.
+ */
+public abstract class OperandCommand implements View.OnClickListener {
+
+    public OperandCommand() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
